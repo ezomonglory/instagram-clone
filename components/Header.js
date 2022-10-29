3a8f7b5466283499af3879f2b7bc53fd23b3bef9
Ezomon Glory <ezomonglory01@gmail.com>
2022-10-29 12:27:59 +0100

    fixed harmburger click

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import {
 	SearchIcon,
@@ -7,26 +7,32 @@ import {
 	HeartIcon,
 	PaperAirplaneIcon,
 	MenuIcon,
+	ArrowSmRightIcon,
+	ArrowSmLeftIcon,
 } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useRecoilState } from "recoil";
-import {modalState} from "../atoms/modalAtom"
+import { modalState } from "../atoms/modalAtom";
 
 function Header() {
 	const { data: session } = useSession();
-    const router = useRouter() 
+	const router = useRouter();
 
-    const [open, setOpen] = useRecoilState(modalState)
+	const [open, setOpen] = useRecoilState(modalState);
+	const [isOpen, setIsOpen] = useState(false);
 
 	return (
 		<div className='shadow-sm border-b bg-white p-2 fixed w-full top-0 z-50'>
 			<div className='flex justify-between max-w-6xl lg:mx-auto mx-5 items-center'>
 				{/* Left */}
-				<div className='relative w-24 h-12 hidden lg:inline-grid cursor-pointer' onClick={()=> {
-                    router.push("/")
-                }}>
+				<div
+					className='relative w-24 h-12 hidden lg:inline-grid cursor-pointer'
+					onClick={() => {
+						router.push("/");
+					}}
+				>
 					<Image
 						src='https://links.papareact.com/ocw'
 						layout='fill'
@@ -34,9 +40,12 @@ function Header() {
 					/>
 				</div>
 
-				<div className='relative w-10 h-10 flex-shrink-0 lg:hidden cursor-pointer' onClick={()=> {
-                    router.push("/")
-                }}>
+				<div
+					className='relative w-10 h-10 flex-shrink-0 lg:hidden cursor-pointer'
+					onClick={() => {
+						router.push("/");
+					}}
+				>
 					<Image
 						src='https://links.papareact.com/jjm'
 						layout='fill'
@@ -57,10 +66,16 @@ function Header() {
 
 				{/* rigth */}
 				<div className='flex items-center justify-center space-x-4'>
-					<HomeIcon className='navBtn' onClick={()=> {
-                    router.push("/")
-                }} />
-					<MenuIcon className='h-6 md:hidden cursor flex-shrink-0' />
+					<HomeIcon
+						className='navBtn'
+						onClick={() => {
+							router.push("/");
+						}}
+					/>
+					<MenuIcon
+						className='h-6 md:hidden cursor flex-shrink-0'
+						onClick={() => setIsOpen(!isOpen)}
+					/>
 
 					{session ? (
 						<>
@@ -70,11 +85,14 @@ function Header() {
 								</div>
 								<PaperAirplaneIcon className='navBtn rotate-45' />
 							</div>
-							<PlusCircleIcon onClick={()=> setOpen(true)} className='navBtn' />
+							<PlusCircleIcon
+								onClick={() => setOpen(true)}
+								className='navBtn'
+							/>
 							<UserGroupIcon className='navBtn' />
 							<HeartIcon className='navBtn' />
 							<img
-                            onClick={signOut}
+								onClick={signOut}
 								src={session.user.image}
 								className='rounded-ful w-10 h-10 rounded-full flex-shrink-0 cursor-pointer'
 							/>
@@ -84,6 +102,52 @@ function Header() {
 					)}
 				</div>
 			</div>
+
+			{isOpen && (
+				<div className='text-black flex flex-col  justify-start ml-5 mt-8 space-y-4'>
+					{session ? (
+						<>
+							<div onClick={signOut} className='flex space-x-2 items-center '>
+								<img
+									src={session.user.image}
+									className='rounded-ful w-10 h-10 rounded-full flex-shrink-0 cursor-pointer'
+								/>
+								<p> {session.user.username} </p>
+							</div>
+							<div
+								onClick={() => setOpen(true)}
+								className='flex space-x-2 items-center'
+							>
+								<PlusCircleIcon className='navBtn !block' />
+								<p>Upload</p>
+							</div>
+
+							<div onClick={signOut} className='flex space-x-2 items-center'>
+								<ArrowSmLeftIcon className='navBtn !block' />
+								Sign Out
+							</div>
+						</>
+					) : (
+						<>
+							<div
+								className='flex space-x-2 items-center'
+								onClick={() => {
+									router.push("/");
+								}}
+							>
+								<HomeIcon className='navBtn !block' />
+
+								<p>Home</p>
+							</div>
+
+							<div onClick={signIn} className='flex space-x-2 items-center'>
+								<ArrowSmRightIcon className='navBtn !block' />
+								Sign In
+							</div>
+						</>
+					)}
+				</div>
+			)}
 		</div>
 	);
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,12 +14,14 @@ export const authOptions = {
     signIn:"/auth/signin"
    },
 
+
    callbacks:{
     async session({session, token, user}){
         session.user.username = session.user.name.split(" ").join("").toLocaleLowerCase();
         session.user.uid = token.sub;
         return session
     }
-   }
+   },
+   
 }
 export default NextAuth(authOptions)
